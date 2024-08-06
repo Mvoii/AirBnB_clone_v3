e0a155d7e378bef945d29b559f9c9d90a63cfcb8
Franklin Mvoi <mvoifranklin@gmail.com>
2024-08-06 03:31:36 +0300

    cities, city skyline

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -8,7 +8,7 @@ from models.state import State
 
 
 @app_views.route('/states', methods=['GET'], strict_slashes=False)
-def _get_states():
+def get_states():
     """get all states objects"""
     state_objs = storage.all(State)
     return jsonify([state.to_dict() for state in state_objs.values()])
@@ -17,17 +17,16 @@ def _get_states():
 @app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
 def get_state_by_id(state_id):
     """get state by id"""
-    state = storage.get(State, state_id)
+    state = storage.get("State", state_id)
     if state is None:
         abort(404)
     return jsonify(state.to_dict())
 
 
-@app_views.route('/states/<state_id>', methods=['DELETE'],
-                 strict_slashes=False)
+@app_views.route('/states/<state_id>', methods=['DELETE'], strict_slashes=False)
 def delete_state_by_id(state_id):
     """delete state by id"""
-    state = storage.get(State, state_id)
+    state = storage.get("State", state_id)
     if state is None:
         abort(404)
     storage.delete(state)
@@ -52,7 +51,7 @@ def create_state():
 @app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
 def update_state(state_id):
     """update state"""
-    state = storage.get(State, state_id)
+    state = storage.get("State", state_id)
     if state is None:
         abort(404)
     data = request.get_json()
